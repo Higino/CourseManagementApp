3d5a5fb543f8a8ec14f28e9bb366fa8b1e05ab79
Higino Silva <higino@gmail.com>
2021-05-26 20:15:25 +0100

    Increase the maximum payload to 50MB

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ const CourseRepository = require('./model/CourseRepository')
 
 const express = require("express");
 const app = express();
+
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
